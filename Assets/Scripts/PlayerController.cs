fe36f981cbdfb70d94ee6bf6ff7b341da2085524
Daniel Grabczewski <danielgrabczewski@gmail.com>
2024-05-30 14:46:25 +1200

    fine tune dash

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,15 +8,21 @@ public class PlayerController : MonoBehaviour
     public float jumpForce = 5f;
     public float dashSpeed = 10f;
     public float dashDuration = 0.2f;
+    public float dashCooldown = 0.5f;
 
     private Rigidbody2D rb;
     private bool isGrounded;
     private bool isDashing;
+    private bool canDash = true;
     private float dashTimeLeft;
+    private float dashCooldownTimer;
+    private float originalGravityScale;
+    private float dashStartY;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        originalGravityScale = rb.gravityScale;
     }
 
     void Update()
@@ -28,6 +34,7 @@ public class PlayerController : MonoBehaviour
         }
 
         HandleDash();
+        UpdateDashCooldown();
     }
 
     void Move()
@@ -38,7 +45,7 @@ public class PlayerController : MonoBehaviour
 
     void Jump()
     {
-        if ((Input.GetKeyDown(KeyCode.W)) && isGrounded)
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded)
         {
             rb.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
             isGrounded = false;
@@ -47,16 +54,12 @@ public class PlayerController : MonoBehaviour
 
     void HandleDash()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (canDash && Input.GetKeyDown(KeyCode.Space))
         {
             float moveInput = Input.GetAxis("Horizontal");
-            if (moveInput > 0) // Dashing to the right
+            if (moveInput != 0) // Dashing in the direction of movement
             {
-                StartDash(Vector2.right);
-            }
-            else if (moveInput < 0) // Dashing to the left
-            {
-                StartDash(Vector2.left);
+                StartDash(new Vector2(moveInput, 0).normalized);
             }
         }
 
@@ -65,10 +68,15 @@ public class PlayerController : MonoBehaviour
             if (dashTimeLeft > 0)
             {
                 dashTimeLeft -= Time.deltaTime;
+                rb.velocity = new Vector2(rb.velocity.x, 0); // Lock the y-position during the dash
+                rb.gravityScale = 0; // Disable gravity during the dash
+                rb.position = new Vector2(rb.position.x, dashStartY); // Maintain the y-position
             }
             else
             {
                 isDashing = false;
+                rb.gravityScale = originalGravityScale; // Restore gravity after the dash
+                StartDashCooldown();
             }
         }
     }
@@ -76,8 +84,30 @@ public class PlayerController : MonoBehaviour
     void StartDash(Vector2 direction)
     {
         isDashing = true;
+        canDash = false; // Disable dashing immediately when starting a dash
         dashTimeLeft = dashDuration;
+        dashStartY = rb.position.y; // Lock the y-position at the start of the dash
         rb.velocity = direction * dashSpeed;
+    }
+
+    void StartDashCooldown()
+    {
+        dashCooldownTimer = dashCooldown;
+    }
+
+    void UpdateDashCooldown()
+    {
+        if (!canDash && !isDashing)
+        {
+            if (dashCooldownTimer > 0)
+            {
+                dashCooldownTimer -= Time.deltaTime;
+            }
+            else
+            {
+                canDash = true;
+            }
+        }
     }
 
     void OnCollisionEnter2D(Collision2D collision)
