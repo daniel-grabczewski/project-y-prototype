64d09b1ae8265fce27de5dc61d6dfa96564030fc
Daniel Grabczewski <danielgrabczewski@gmail.com>
2024-06-01 13:20:54 +1200

    add buffer between changing directions to avoid idle animation flicker

--- Assets/Scripts/Core/Player/PlayerAnimationController.cs
+++ Assets/Scripts/Core/Player/PlayerAnimationController.cs
@@ -8,6 +8,9 @@ public class PlayerAnimationController : MonoBehaviour
     private SpriteRenderer spriteRenderer;
     private Vector2 movement;
     private bool facingRight = true;
+    private float idleBufferTime = 0.1f; // Buffer time in seconds
+    private float idleBufferCounter = 0f;
+    private KeyCode lastKeyPressed = KeyCode.None;
 
     void Start()
     {
@@ -24,29 +27,53 @@ public class PlayerAnimationController : MonoBehaviour
 
     void HandleMovement()
     {
-        movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+        movement = new Vector2(horizontalInput, Input.GetAxisRaw("Vertical"));
         animator.SetFloat("Speed", Mathf.Abs(movement.x));
+
+        if (Mathf.Abs(horizontalInput) > 0.1f)
+        {
+            idleBufferCounter = idleBufferTime; // Reset the buffer when there is input
+            lastKeyPressed = horizontalInput > 0 ? KeyCode.D : KeyCode.A;
+        }
+
+        if (horizontalInput != 0)
+        {
+            animator.SetBool("isRunning", true);
+            animator.SetBool("isIdle", false);
+        }
     }
 
     void HandleIdle()
     {
-        if (movement.x == 0)
+        if (Mathf.Abs(movement.x) < 0.1f)
         {
-            animator.SetBool("isIdle", true);
+            if (idleBufferCounter > 0)
+            {
+                idleBufferCounter -= Time.deltaTime;
+                animator.SetBool("isRunning", true);
+                animator.SetBool("isIdle", false);
+            }
+            else
+            {
+                animator.SetBool("isRunning", false);
+                animator.SetBool("isIdle", true);
+            }
         }
         else
         {
+            idleBufferCounter = idleBufferTime; // Reset the buffer when there is movement
             animator.SetBool("isIdle", false);
         }
     }
 
     void FlipSprite()
     {
-        if (movement.x > 0 && !facingRight)
+        if (lastKeyPressed == KeyCode.D && !facingRight)
         {
             Flip();
         }
-        else if (movement.x < 0 && facingRight)
+        else if (lastKeyPressed == KeyCode.A && facingRight)
         {
             Flip();
         }
